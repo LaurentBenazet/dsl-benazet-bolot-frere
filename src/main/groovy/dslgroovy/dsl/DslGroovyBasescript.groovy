d5718963d43aeac13e3f0feb979434db7df94e77
Laurent Benazet <laurent-jerome.benazet-lacarre@etu.univ-cotedazur.fr>
2020-01-15 11:28:41 +0100

    completed code

--- src/main/groovy/dslgroovy/dsl/DslGroovyBasescript.groovy
+++ src/main/groovy/dslgroovy/dsl/DslGroovyBasescript.groovy
@@ -1,4 +1,77 @@
 package dslgroovy.dsl
 
-class DslGroovyBasescript {
+import io.github.mosser.arduinoml.kernel.behavioral.Action
+import io.github.mosser.arduinoml.kernel.behavioral.State
+import io.github.mosser.arduinoml.kernel.structural.Actuator
+import io.github.mosser.arduinoml.kernel.structural.SIGNAL
+import io.github.mosser.arduinoml.kernel.structural.Sensor
+
+abstract class DslGroovyBasescript extends Script {
+    // sensor "name" pin n
+    def sensor(String name) {
+        [pin  : { n -> ((DslGroovyBinding) this.getBinding()).getModel().createSensor(name, n) },
+         onPin: { n -> ((DslGroovyBinding) this.getBinding()).getModel().createSensor(name, n) }]
+    }
+
+    // actuator "name" pin n
+    def actuator(String name) {
+        [pin: { n -> ((DslGroovyBinding) this.getBinding()).getModel().createActuator(name, n) }]
+    }
+
+    // state "name" means actuator becomes signal [and actuator becomes signal]*n
+    def state(String name) {
+        List<Action> actions = new ArrayList<Action>()
+        ((DslGroovyBinding) this.getBinding()).getModel().createState(name, actions)
+        // recursive closure to allow multiple and statements
+        def closure
+        closure = { actuator ->
+            [becomes: { signal ->
+                Action action = new Action()
+                action.setActuator(actuator instanceof String ? (Actuator) ((DslGroovyBinding) this.getBinding()).getVariable(actuator) : (Actuator) actuator)
+                action.setValue(signal instanceof String ? (SIGNAL) ((DslGroovyBinding) this.getBinding()).getVariable(signal) : (SIGNAL) signal)
+                actions.add(action)
+                [and: closure]
+            }]
+        }
+        [means: closure]
+    }
+
+    // initial state
+    def initial(state) {
+        ((DslGroovyBinding) this.getBinding()).getModel().setInitialState(state instanceof String ? (State) ((DslGroovyBinding) this.getBinding()).getVariable(state) : (State) state)
+    }
+
+    // from state1 to state2 when sensor becomes signal
+    def from(state1) {
+        [to: { state2 ->
+            [when: { sensor ->
+                [becomes: { signal ->
+                    ((DslGroovyBinding) this.getBinding()).getModel().createTransition(
+                            state1 instanceof String ? (State) ((DslGroovyBinding) this.getBinding()).getVariable(state1) : (State) state1,
+                            state2 instanceof String ? (State) ((DslGroovyBinding) this.getBinding()).getVariable(state2) : (State) state2,
+                            sensor instanceof String ? (Sensor) ((DslGroovyBinding) this.getBinding()).getVariable(sensor) : (Sensor) sensor,
+                            signal instanceof String ? (SIGNAL) ((DslGroovyBinding) this.getBinding()).getVariable(signal) : (SIGNAL) signal)
+                }]
+            }]
+        }]
+    }
+
+    // export name
+    def export(String name) {
+        println(((DslGroovyBinding) this.getBinding()).getModel().generateCode(name).toString())
+    }
+
+    // disable run method while running
+    int count = 0
+
+    abstract void scriptBody()
+
+    def run() {
+        if (count == 0) {
+            count++
+            scriptBody()
+        } else {
+            println "Run method is disabled"
+        }
+    }
 }
--- src/main/groovy/dslgroovy/dsl/DslGroovyDSL.groovy
+++ src/main/groovy/dslgroovy/dsl/DslGroovyDSL.groovy
@@ -1,8 +1,58 @@
 package dslgroovy.dsl
 
+import io.github.mosser.arduinoml.kernel.structural.SIGNAL
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.SecureASTCustomizer
+
 class DslGroovyDSL {
     private GroovyShell shell
+    private CompilerConfiguration configuration
     private DslGroovyBinding binding
+    private DslGroovyBasescript basescript
+
+    DslGroovyDSL() {
+        binding = new DslGroovyBinding()
+        binding.setModel(new DslGroovyModel(binding));
+        configuration = getDSLConfiguration()
+        configuration.setScriptBaseClass("dslgroovy.dsl.DslGroovyBasescript")
+        shell = new GroovyShell(configuration)
+
+        binding.setVariable("high", SIGNAL.HIGH)
+        binding.setVariable("low", SIGNAL.LOW)
+    }
+
+    private static CompilerConfiguration getDSLConfiguration() {
+        def secure = new SecureASTCustomizer()
+        secure.with {
+            //disallow closure creation
+            closuresAllowed = false
+            //disallow method definitions
+            methodDefinitionAllowed = true
+            //empty white list => forbid imports
+            importsWhitelist = [
+                    'java.lang.*'
+            ]
+            staticImportsWhitelist = []
+            staticStarImportsWhitelist= []
+            //language tokens disallowed
+//			tokensBlacklist= []
+            //language tokens allowed
+            tokensWhitelist= []
+            //types allowed to be used  (including primitive types)
+            constantTypesClassesWhiteList= [
+                    int, Integer, Number, Integer.TYPE, String, Object
+            ]
+            //classes who are allowed to be receivers of method calls
+            receiversClassesWhiteList= [
+                    int, Number, Integer, String, Object
+            ]
+        }
+
+        def configuration = new CompilerConfiguration()
+        configuration.addCompilationCustomizers(secure)
+
+        return configuration
+    }
 
     void eval(File scriptFile) {
         Script script = shell.parse(scriptFile)
